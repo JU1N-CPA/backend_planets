547098d246be31590c3665fa0ae1afbb7669e17e
juan.palacio@vindow.com <juan.palacio@vindow.com>
2024-08-26 13:00:37 +0200

    fixing bugs update

--- core/planets/urls.py
+++ core/planets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import FetchAndSavePlanetsView,AllPlanetsView,RetrievePlanetByNameView,CreatePlanetView,DeletePlanetView
+from .views import FetchAndSavePlanetsView,AllPlanetsView,RetrievePlanetByNameView,CreatePlanetView,DeletePlanetView,UpdatePlanetView
 
 urlpatterns = [
     path('Fetchplanets/', FetchAndSavePlanetsView.as_view(), name='fetch_planets'),
     path('AllPlanets/', AllPlanetsView.as_view(), name='retrieve_all_planets'),
     path('planetview/<str:name>/', RetrievePlanetByNameView.as_view(), name='retrieve-planet'),
     path('planets/create/', CreatePlanetView.as_view(), name='create-planet'),
-    path('planets/delete/<str:name>/', DeletePlanetView.as_view(), name='delete-planet')
+    path('planets/delete/<str:name>/', DeletePlanetView.as_view(), name='delete-planet'),
+    path('planets/update/<str:name>/', UpdatePlanetView.as_view(), name='update-planet')
+
 ]
